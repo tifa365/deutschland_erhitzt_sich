019b99059e54b2192e1d5c2e8b326d993d33ba6c
tifa365 <tfangmeyer@gmail.com>
2022-08-31 11:12:16 +0200

    all new files

--- climate_multiples.R
+++ climate_multiples.R
@@ -5,85 +5,115 @@ library(sf)
 library(scico)
 library(hrbrthemes)
 library(classInt)
-# create folder to store data in project
-dir.create("data")
+library(rdwd)
+library(purrr)
+library(leaflet)
+library(lubridate)
+library(gganimate)
+# prepare temperature data
 
-# we have data for the following years:
-years <- 1951L:2021L
+# download weather station list to decide from which station data to pull from
+mydatapath <- "./data/"
 
-# ... and start download it:
-walk(years, function(year){
-  download.file(
-    glue("https://opendata.dwd.de/climate_environment/CDC/grids_germany/annual/hot_days/grids_germany_annual_hot_days_{year}_17.asc.gz"),
-    destfile = glue("data/grids_germany_annual_hot_days_{year}.asc.gz")
-  )
-  R.utils::gunzip(glue("data/grids_germany_annual_hot_days_{year}.asc.gz"), remove=TRUE)
-})
+# opendata_dwd_path <- "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/recent/"
+# 
+# download.file(paste0(opendata_dwd_path, "KL_Tageswerte_Beschreibung_Stationen.txt"),
+#               paste0(mydatapath, "weatherstations.txt"))
 
-# list all raster 
-hot_raster <- list.files(path = "data/", full.names = TRUE)
+header <- read_table(file = paste0(mydatapath, "weatherstations.txt"),
+                      col_names = FALSE,
+                      n_max = 1)
+weatherdata <- read_table(file = paste0(mydatapath, "weatherstations.txt"),
+                          col_names = FALSE,
+                          locale = locale(encoding = "WINDOWS-1252"),
+                          skip = 2)
+colnames(weatherdata) <- header[1, ]
 
-# and read them via terra:
-hot_raster <- rast(hot_raster)
-names(hot_raster) <- years
+
+# Select only those stations that provide data between 1961 and 2021
+sixty_year_weather_data <- weatherdata |> 
+  select(c(1:7)) |>  
+  filter(von_datum <= as.numeric(19610101)) |>  
+  filter(bis_datum >= as.numeric(20211231)) |> 
+  mutate(Stations_id = as.integer(Stations_id))
+
+# Pull data from one weather station
+save_one_weatherstation <- function(weatherstation_id) {
+  # get DWD download link by weatherstaion id
+  link <- selectDWD(id=toString(weatherstation_id), res="daily", var="kl", per="hist") 
+  
+  # Actually download that dataset, returning the local storage file name:
+  file <- dataDWD(link, read=FALSE)
+  
+  # Read each weatherstation file from the zip folder and save as .rds into ./data/ folder
+  readDWD(file, varnames=TRUE) |> 
+    select(c(STATIONS_ID, MESS_DATUM, TXK.Lufttemperatur_Max)) |>
+    saveRDS(file = glue('./data/', weatherstation_id, '.rds'))
+}
+
+# Use previously filtered weather station ids to pull data from each of them and save to data folder 
+# map(sixty_year_weather_data$Stations_id, save_one_weatherstation)
+
+# Read in .rds files from ./data/ folder
+combined_rds_files <- list.files( path = "./data/", pattern = "*.rds", full.names = TRUE ) %>%
+  map_dfr(readRDS)
+
+# filter only those temperature per day values between 1961 and 2021
+filtered_data_by_timeframe <- combined_rds_files %>%
+  filter(MESS_DATUM >= "1961-01-01", MESS_DATUM <= "2021-01-01")
+
+# prepare spatial data to visualize temperature
 
 # read in counties
-# source of shapefile: https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/verwaltungsgebiete-1-2-500-000-stand-01-01-vg2500.html
-counties <- st_read("vg2500/vg2500_krs.shp")
-counties_vect <- vect(counties)
+# source of shapefile: https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/nuts-gebiete-1-250-000-stand-31-12-nuts250-31-12.html
+counties <- st_read("nuts250_1231/250_NUTS1.shp") 
 
-# reproject to bring the data into the same space
-dwd_crs <- "epsg:31467"
-crs(hot_raster) <- dwd_crs
-hot_raster <- terra::project(hot_raster, dwd_crs)
 
-crs(counties_vect) <- dwd_crs
-counties_vect <- terra::project(counties_vect, dwd_crs)
+station_id_values_per_year <- filtered_data_by_timeframe |>
+  group_by(STATIONS_ID, year = lubridate::floor_date(MESS_DATUM, "year")) |> 
+  summarise(n_days=sum(TXK.Lufttemperatur_Max >= 35)) |> 
+  mutate(n_days = replace_na(n_days, 0)) |> 
+  rename(Stations_id = STATIONS_ID) 
 
-# extract raster values for each into the county polygon
-hot_counties <- terra::extract(hot_raster, 
-                               counties_vect, 
-                               mean, 
-                               na.rm=TRUE)
 
-# make our data tidy and pivot to long
-names(hot_counties) <- c("ARS", years)
-hot_counties$ARS <- counties_vect$ARS
+# filter only those days where the temperature is 35 degrees or more
+# station_id_values_per_year <- filtered_data_by_timeframe |>
+#   #filter(TXK.Lufttemperatur_Max >= 35 && !TXK.Lufttemperatur_Max %in% c(0)) |> 
+#   filter(all(TXK.Lufttemperatur_Max >= 35  | TXK.Lufttemperatur_Max == 0))
+#   group_by(STATIONS_ID, year = lubridate::floor_date(MESS_DATUM, "year")) |> 
+#   count() |> 
+#   rename(Stations_id = STATIONS_ID) |> 
+#   rename(n_days = n) |> 
+#   mutate(n_days = replace_na(n_days, 0))
+  
+# join 'station_id_values_per_year' to also include latitude and longitude for viz
+joined_spatial_df <- left_join(station_id_values_per_year, sixty_year_weather_data, by = "Stations_id") |> 
+  mutate(y = year(year)) |> 
+  # mutate(n_days = na_if(n_days, 0)) |> 
+  # recode(n_days, 0 = 0.00001) |> 
+  # mutate(n_days = replace(n_days, n_days==0, 0.001)) |> 
+  st_as_sf(coords = c("geoLaenge", "geoBreite")) |> 
+  st_set_crs("epsg:4326") |> 
+  st_transform("epsg:31467") |> 
+  mutate(year = year(year)) |> 
+  arrange(Stations_id, year) 
 
-hot_counties_long <- hot_counties %>%
-  pivot_longer(!ARS, names_to = "Year", values_to = "n_days") %>% 
-  mutate(n_days = replace_na(n_days, 0))
+# visualize data
 
-# classify the data - if you change the data to another country
-# this step definitely needs some fine-tuning
-hot_classes <- classIntervals(hot_counties_long$n_days, 
-                              n=8, 
-                              style="fixed",
-                              fixedBreaks=c(-1, 1, 5, 10, 15, 20, 25, 30, 100))
-hot_counties_long$classes <- cut(hot_counties_long$n_days, hot_classes$brks)
-
-# and finally,
-counties_long <- left_join(hot_counties_long, counties, by = c("ARS"))
-
-legend_labels <- c("0 - 1", "2-5", "6-10", "11-15", "16-20", "21-25", "26-30", ">31")
-
-counties_long %>%  
-  st_as_sf() %>% 
-  ggplot() +
-  geom_sf(aes(fill = classes),
-          size = 0.01) +
-  scale_fill_viridis_d(labels= legend_labels, 
-                       option = "F",
-                       direction = -1, 
-                       begin = .17, 
-                       end = .97,
-                       na.value = "#F9DFCCFF") +
-  facet_wrap(Year ~ .,
-             ncol = 11) +
-  labs(title = "Germany's heating-up",
-       #subtitle = "The hottest counties in Germany since 1951. We calculated from a raster source all",
-       fill = "Number of days above 30°C",
-       caption = "Data: BKK & DWD | Visualisation: Marco Sciaini") + 
+p <- ggplot(data = counties) +
+  geom_sf(fill = "white", size=0.15) +
+  geom_sf(data=joined_spatial_df, aes(size = n_days, stroke = 0, alpha = I(ifelse(n_days < 1, 0, 0.7))), color="#E64415") +
+  scale_size_binned(
+    trans = "identity",
+    breaks = c(0, 1, 4, 8, 12, 16),
+    range = c(1, 2.8),
+  )  +
+  # transition_time(year) +
+  facet_wrap(year ~ .,
+             ncol = 10) +
+  labs(title = "Germany is getting hotter",
+       subtitle = "Number of days above 35°C per year",
+       caption = "Data: BKK & DWD | Visualization: @Datenschatz") + 
   guides(fill = guide_legend(keyheight = unit(1, units = "mm"),  
                              keywidth = unit(3, units = "mm"),
                              direction = "horizontal",
@@ -95,21 +125,28 @@ counties_long %>%
   theme_void() +
   theme(legend.direction="horizontal",
         legend.position="bottom", 
+        legend.justification = "left",
         legend.box = "vertical",
-        text=element_text(family="West"),
+        legend.title = element_blank(),
+        text=element_text(family="Times New Roman"),
         panel.spacing.y = unit(1, "lines"),
+        plot.background = element_rect(fill = 'white', colour = 'white'),
+        plot.subtitle = element_text(size=15, 
+                                   vjust=1, 
+                                   color="black",
+                                   margin = margin(.3,0,.5,0, "cm")),
         plot.title = element_text(size = 26, 
                                   face = "bold", 
-                                  colour = '#181c20',
+                                  colour = 'black',
                                   margin = margin(.3,0,.5,0, "cm")),
-        strip.background =element_rect(fill= alpha('#cd5c5c', 0.05), colour = NA),
-        strip.text = element_text(size = 7, 
+        strip.background = element_rect(fill= alpha('#ffffff', 0.05), colour = NA),
+        strip.text = element_text(size = 9, 
                                   face = "bold", 
-                                  colour = '#181c20',
+                                  colour = '#000000',
                                   margin = margin(.1,0,.1,0, "cm")),
-        plot.caption = element_text(size = 7,
-                                    colour = '#181c20',
-                                    margin = margin(.3,0,.3,0, "cm")))
+        plot.caption = element_text(size = 9,
+                                    colour = '#000000',
+                                    margin = margin(.5,0,.3,0, "cm")))
 
+ggsave("days_above_35_degrees_yearly.jpg", dpi = 150, width = 1200, height = 1200, units = "px")
 
-ggsave("germany_heating_up.png", dpi = 300, width = 2000, height = 2400, units = "px")
